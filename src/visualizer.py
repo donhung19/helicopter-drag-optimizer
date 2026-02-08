beb08df27c5431fcead68ff9f680b7c9910e8784
donhung19 <nhungdo370@gmail.com>
2026-02-08 15:53:24 +0700

    update src

--- src/visualizer.py
+++ src/visualizer.py
@@ -28,7 +28,7 @@ class Visualizer:
         n_rows = math.ceil(total_models / max_cols)
         if total_models == 0:
             return None
-        fig, axs = plt.subplots(n_rows, n_cols, figsize=(6 * n_cols, 5 * n_rows), dpi=120, sharey=False, squeeze=False)
+        fig, axs = plt.subplots(n_rows, n_cols, figsize=(9 * n_cols, 9 * n_rows), dpi=120, sharey=False, squeeze=False)
 
         for index, obj in enumerate(model_lists):
             row_idx = index // n_cols  # Chia lấy phần nguyên để tìm hàng
@@ -53,6 +53,23 @@ class Visualizer:
                         xy=(v_opt, d_min), 
                         xytext=(v_opt + 5, d_min + 500), # Đẩy chữ ra xa một chút
                         arrowprops=dict(arrowstyle='->', color='red'))
+            
+            table_data = [
+                ["Weight (N)", f"{obj.weight:,.0f}"],
+                ["R Radius (m)", f"{obj.rotor_radius:.1f}"],
+                ["f Factor", f"{obj.f_factor:.3f}"],
+                ["k Factor", f"{obj.k_factor:.2f}"],
+                ["Rho (kg/m3)", f"{obj.rho:.3f}"]
+            ]
+            the_table = ax.table(
+                cellText=table_data,
+                colLabels=["Parameter", "Value"],
+                loc='bottom',
+                cellLoc='center',
+                bbox=[0, -0.5, 1, 0.3] # Đẩy bảng xuống dưới trục X
+            )
+            the_table.auto_set_font_size(False)
+            the_table.set_fontsize(8)
 
         plt.tight_layout()
         #plt.show()
